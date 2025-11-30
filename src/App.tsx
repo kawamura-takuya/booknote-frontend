8e5d89b58407e50277b1eeaa832132b32fb6ef69
calmdown1987 <calmdown1987@icloud.com>
2025-11-30 09:52:47 +0900

    新規登録ページへのルーティングを追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home/Home";
+import Signup from "./pages/Signup/Signup";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/signup" element={<Signup />} />
       </Routes>
     </Router>
   );
